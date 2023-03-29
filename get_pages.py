58d94247642bfd05190f891de506e3737978b63c
R Yallow <yallowrvn@gmail.com>
2023-03-29 23:10:10 +0200

    add url validation

--- get_pages.py
+++ get_pages.py
@@ -1,15 +1,24 @@
 import argparse
 import requests
 
-parser = argparse.ArgumentParser(description='Fetch the HTML contents of a webpage')
-parser.add_argument('url', type=str, help='URL of the webpage to fetch')
+parser = argparse.ArgumentParser(description='Download music from the Free Music Archive')
+parser.add_argument('url', type=str, help='URL of the webpage to download music from')
 
 args = parser.parse_args()
 
 url = args.url
+
+if not url.startswith('https://freemusicarchive.org/'):
+    print("URL must start with 'https://freemusicarchive.org/'")
+    exit()
+
 response = requests.get(url)
 
 if response.status_code != 200:
-    print("Failed to retrieve HTML content")
+    print("Failed to download music")
+    exit()
 
-content = response.content
+music = response.content
+# Do something with the downloaded music
+
+print("Music downloaded successfully")
