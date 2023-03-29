b098e01ac35af5b0da19b147fda86aca1fc867f3
R Yallow <yallowrvn@gmail.com>
2023-03-29 23:42:56 +0200

    extract url validation

---
+++ url_validation.py
@@ -0,0 +1,38 @@
+import re
+import requests
+from urllib.parse import urlparse, urlunparse
+
+from enum import Enum
+
+
+class UrlValidationResult(Enum):
+    VALID = 0
+    INVALID_URL = 1
+    INVALID_PAGE = 2
+
+
+class UrlValidationErrors(Enum):
+    INVALID_URL = "URL must start with 'https://freemusicarchive.org/'"
+    INVALID_PAGE = "Error: this is not a music listing page"
+
+def get_error_message(result):
+    return {
+        UrlValidationResult.INVALID_URL: UrlValidationErrors.INVALID_URL.value,
+        UrlValidationResult.INVALID_PAGE: UrlValidationErrors.INVALID_PAGE.value,
+    }.get(result, "Unknown error")
+
+def validate_url(url):
+    if not url.startswith('https://freemusicarchive.org/'):
+        return UrlValidationResult.INVALID_URL
+
+    # Remove query parameters from URL
+    parsed_url = urlparse(url)._replace(query=None)
+    url = urlunparse(parsed_url)
+
+    response = requests.get(url)
+    page = response.content.decode()
+
+    if not re.search(r'<div class="play-item[^\n]*', page):
+        return UrlValidationResult.INVALID_PAGE
+
+    return UrlValidationResult.VALID
